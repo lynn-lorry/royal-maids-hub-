b21d49966feb88e112bbee571bd82f84f8648919
lynn-lorry <scolarlynn@gmail.com>
2024-07-24 10:22:59 +0300

    this is the work

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ setInterval(() => {
   index = (index + 1) % images.length;
   images[index].style.opacity = 1;
 }, 3000); // 3000ms = 3 seconds
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const prevButton = document.querySelector('.carousel-prev');
+const nextButton = document.querySelector('.carousel-next');
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateSlides();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateSlides();
+});
+
+function updateSlides() {
+  const translateX = -currentIndex * 100;
+  document.querySelector('.carousel-slides').style.transform = `translateX(${translateX}%)`;
+}
